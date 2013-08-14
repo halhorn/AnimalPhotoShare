6e422429e5b09ac65c2f30f03c871e109508bf78
信田 春満 <halhorn@halmidi.com>
2013-08-14 21:11:56 +0900

    add table view cell

---
+++ HALAnimalPhotoShare/ViewController/Component/HALNormalTimelineTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HALNormalTimelineTableViewCell.h
+//  HALAnimalPhotoShare
+//
+//  Created by 信田 春満 on 2013/08/14.
+//  Copyright (c) 2013年 halhorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HALNormalTimelineTableViewCell : UITableViewCell
+
+@end
---
+++ HALAnimalPhotoShare/ViewController/Component/HALNormalTimelineTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  HALNormalTimelineTableViewCell.m
+//  HALAnimalPhotoShare
+//
+//  Created by 信田 春満 on 2013/08/14.
+//  Copyright (c) 2013年 halhorn. All rights reserved.
+//
+
+#import "HALNormalTimelineTableViewCell.h"
+
+@implementation HALNormalTimelineTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
